5ff18850cd13b5eabcabe06058fcf6344610910f
AleksSh <Coluchy@yandex.ru>
2024-05-26 18:59:22 +0600

    first commit

---
+++ src/test/java/pages/ContactsPage.java
@@ -0,0 +1,63 @@
+package pages.components;
+
+
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class ContactsPage {
+    private final SelenideElement
+            regionList = $(".contacts-tags"),
+            mapInfoCollapsible = $$(".collapse-link").find(visible),
+            pressEmail = $(".press-contacts").$(byText("E-mail")).parent(),
+            pressPhoneNumber = $(".press-contacts").$(byText("Телефон")).parent();
+    private final ElementsCollection addressItems = $$("[itemprop=address]"),
+            mapInfoAddressItems = $$(".map-info").find(visible).$$("[itemprop=address]");
+
+    @Step("Открываем страницу контактов")
+    public ContactsPage openPage() {
+        open("/contacts");
+        return this;
+    }
+
+    @Step("Выбираем регион '{value}'")
+    public ContactsPage clickRegion(String value) {
+        regionList.$(byText(value)).click();
+        return this;
+    }
+
+    @Step("Раскрываем раздел с юридическим адресом (есть в некоторых городах)")
+    public ContactsPage clickMapInfo() {
+        mapInfoCollapsible.scrollIntoView("{block: \"center\", inline: \"center\"}").click();
+        return this;
+    }
+
+    @Step("Проверяем, что отображается адрес '{value}'")
+    public ContactsPage checkVisibleAddress(String value) {
+        addressItems.findBy(text(value)).shouldBe(visible).scrollIntoView("{block: \"center\", inline: \"center\"}");
+        return this;
+    }
+
+    @Step("Проверяем, что отображается юридический адрес '{value}'")
+    public ContactsPage checkVisibleMapInfo(String value) {
+        mapInfoAddressItems.findBy(text(value)).shouldBe(visible).scrollIntoView("{block: \"center\", inline: \"center\"}");
+        return this;
+    }
+
+    @Step("Проверяем, что у пресс-службы адрес почты '{value}'")
+    public ContactsPage checkPressEmail(String value) {
+        pressEmail.scrollIntoView("{block: \"center\", inline: \"center\"}").shouldHave(text(value));
+        return this;
+    }
+
+    @Step("Проверяем, что у пресс-службы номер телефона '{value}'")
+    public ContactsPage checkPressPhoneNumber(String value) {
+        pressPhoneNumber.scrollTo().scrollIntoView("{block: \"center\", inline: \"center\"}").shouldHave(text(value));
+        return this;
+    }
+}
---
+++ src/test/java/pages/ContactsPageTests.java
@@ -0,0 +1,60 @@
+package tests;
+
+import io.qameta.allure.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import pages.ContactsPage;
+
+@Epic("UI сайта IBS.ru")
+@Feature("Страница контактов")
+@DisplayName("Сценарии проверок страницы контактов")
+public class ContactsPageTests extends TestBase {
+    ContactsPage contactsPage = new ContactsPage();
+    String pressEmail = "pressa@ibs.ru",
+            pressPhoneNumber = "+7 (495) 967-80-80";
+
+    @Story("Список контактов по городам")
+    @DisplayName("Адреса: ")
+    @ParameterizedTest(name = "Проверка отображения адреса {1} в городе {0}")
+    @CsvSource(delimiter = '|', value = {
+            "Санкт-Петербург | Английская набережная, д. 70",
+            "Вологда | ул. Зосимовская, д.18",
+            "Уфа | ул. Менделеева, д. 130, блок А, 2 этаж, офис 201"
+    })
+    @Severity(SeverityLevel.BLOCKER)
+    @Owner("aydarium")
+    void regionSelectionTest(String city, String address) {
+        contactsPage.openPage()
+                .clickRegion(city)
+                .checkVisibleAddress(address);
+    }
+
+    @Story("Дополнительные контакты в городах")
+    @DisplayName("Юридические адреса: ")
+    @ParameterizedTest(name = "Проверка отображения юридического адреса {1} в городе {0}")
+    @CsvSource(delimiter = '|', value = {
+            "Москва | Дмитровское шоссе, 9Б",
+            "Санкт-Петербург | ул. Галерная, д. 73, Лит. А",
+            "Ульяновск | ул. Марата д.8Б, 3 этаж"
+    })
+    @Severity(SeverityLevel.NORMAL)
+    @Owner("aydarium")
+    void mapInfoCollapsibleTest(String city, String address){
+        contactsPage.openPage()
+                .clickRegion(city)
+                .clickMapInfo()
+                .checkVisibleMapInfo(address);
+    }
+
+    @Test
+    @DisplayName("Проверка отображения адреса электронной почты и номера телефона пресс-службы")
+    @Severity(SeverityLevel.NORMAL)
+    @Owner("aydarium")
+    void pressContactsTest() {
+        contactsPage.openPage()
+                .checkPressEmail(pressEmail)
+                .checkPressPhoneNumber(pressPhoneNumber);
+    }
+}
