eb6d6a3e74b4c80370f799c023a6db4f76790e6e
AleksSh <Coluchy@yandex.ru>
2024-05-26 22:49:58 +0600

    fixes added

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -3,46 +3,45 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
-import helpers.Attach;
-import org.junit.jupiter.api.BeforeAll;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import config.DriverConfig;
+import helpers.Attach;
+
 import java.util.Map;
+import java.util.Objects;
 
 public class TestBase {
     @BeforeAll
     static void beforeAll() {
-
-        Configuration.baseUrl = "https://ibs.ru/";
-        Configuration.pageLoadStrategy = "normal";
-        Configuration.timeout = 5000;
-        Configuration.browser = "chrome";
-        Configuration.browserVersion = "100.0";
-        Configuration.browserSize = "1920x1080";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
-                "enableVNC", true,
-                "enableVideo", true
-        ));
-        Configuration.browserCapabilities = capabilities;
-    }
-
-    @BeforeEach
-    void beforeEach() {
+        DriverConfig driverConfig = ConfigFactory.create(DriverConfig.class);
+        Configuration.baseUrl = "https://ibs.ru";
+        Configuration.pageLoadStrategy = driverConfig.pageLoadStrategy();
+        Configuration.browser = driverConfig.browser();
+        Configuration.browserVersion = driverConfig.browserVersion();
+        Configuration.browserSize = driverConfig.browserSize();
         SelenideLogger.addListener("allure", new AllureSelenide());
+        if (driverConfig.isRemote()) {
+            Configuration.remote = driverConfig.browserRemoteUrl();
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                    "enableVNC", true,
+                    "enableVideo", true
+            ));
+            Configuration.browserCapabilities = capabilities;
+        }
     }
 
     @AfterEach
     void addAttachments() {
-        Attach.screenshotAs("Last screenshot");
+        Attach.screenshotAs("Финальный скриншот");
         Attach.pageSource();
-        Attach.browserConsoleLogs();
-        Attach.addVideo();
+        if (!Objects.equals(Configuration.browser, "firefox")) Attach.browserConsoleLogs();
+        if (Configuration.remote != null) Attach.addVideo();
         Selenide.closeWebDriver();
     }
 }
